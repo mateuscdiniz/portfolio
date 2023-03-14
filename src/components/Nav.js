7c380efa2008cd044302f1b611370c15d8b8eb26
mateus <mateuscdiniz@gmail.com>
2023-03-14 17:47:39 -0300

    creates navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
   return (
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx-auto"></div>
-      <div className="w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full items-center max-w-[460px] mx-auto px-5 flex justify-between text-2x1 text-white/50">
+      <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full items-center max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50">
         <Link
           to="home"
           activeClass="active"
